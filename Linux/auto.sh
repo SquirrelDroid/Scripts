a63717d4f3038c663db92920a3c09206b876dd48
Conner <conner-m@users.noreply.github.com>
2017-08-24 21:48:54 -0700

    Small changes and a couple additional features
    Formatted output, looks cleaner.
    Clears dpkg cache (Had trouble with dpkg being locked, not allowing manipulation of the apt system)
    Added sshd config (Restricted root login)
    Removes ftp service (vsftpd)
    Scans and notifies of possible backdoors (netcat)

--- Linux/auto.sh
+++ Linux/auto.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
 
-sudo apt-get install ufw gufw libpam-cracklib auditd bum 
+DPKG="$(sudo fuser /var/lib/dpkg/lock)"
+if [ -z "$DPKG" ]; then
+	echo "\ndpkg locked"
+	echo -n "Removing dpkg cache"
+
+	if [ -e /var/lib/apt/lists/lock ]; then
+		sudo rm /var/lib/apt/lists/lock
+	fi
+	echo -n "."
+
+	if [ -e /var/cache/apt/archives/lock ]; then
+		sudo rm /var/cache/apt/archives/lock
+	fi
+	echo -n "."
+
+	if [ -e /var/lib/dpkg/lock ]; then
+		sudo rm /var/lib/dpkg/lock
+	fi
+	echo -n "."
+fi
+
+echo "\ndpkg lock clear\n\n"
+
+echo "Installing ufw, gufw, libpam-cracklib, auditd, bum"
+sudo apt-get --assume-yes install ufw gufw libpam-cracklib auditd bum 
+echo "Done."
+
+echo "\n\nConfiguring firewall"
 sudo ufw default deny incoming && sudo ufw default allow outgoing && sudo ufw enable
+echo "Done."
+
+echo -n "\nConfiguring common-password file."
 sudo 	echo "password	requisite			pam_cracklib.so retry=3 minlen=8 difok=3 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1
 		password	[success=1 default=ignore]	pam_unix.so obscure use_authtok try_first_pass sha512 remember=5 minlen=8
 		password	requisite			pam_deny.so
 		password	required			pam_permit.so
-		password	optional	pam_gnome_keyring.so " > /etc/pam.d/common-password	
-echo "Common-password - "
+		password	optional	pam_gnome_keyring.so " > tmp
+echo -n "."
+sudo cp tmp /etc/pam.d/common-password
+echo "."	
+sudo rm tmp
+echo "Done."
 
+echo -n "\nConfiguring login policies."
 sudo echo "MAIL_DIR        /var/mail
 		FAILLOG_ENAB		yes
 		LOG_UNKFAIL_ENAB	no
@@ -39,15 +74,25 @@ sudo echo "MAIL_DIR        /var/mail
 		CHFN_RESTRICT		rwh
 		DEFAULT_HOME	yes
 		USERGROUPS_ENAB yes
-		ENCRYPT_METHOD SHA512" > /etc/login.defs
-echo "Login - "
+		ENCRYPT_METHOD SHA512" > tmp
+echo -n "."
+sudo cp tmp /etc/login.defs
+echo "."
+sudo rm tmp
+echo "Done."
 
+echo -n "\nConfiguring common-auth."
 sudo echo "auth	[success=1 default=ignore]	pam_unix.so nullok_secure
 		auth	requisite			pam_deny.so
 		auth	required			pam_permit.so
-		auth  	required			pam_tally2.so deny=5 onerr=fail unlock_time=1" > /etc/pam.d/common-auth
-echo "Common-Auth - "
+		auth  	required			pam_tally2.so deny=5 onerr=fail unlock_time=1" > tmp
+echo -n "."
+sudo cp tmp /etc/pam.d/common-auth
+echo "."
+sudo rm tmp
+echo "Done."
 
+echo -n "\nConfiguring auditing."
 sudo echo "log_file = /var/log/audit/audit.log
 		log_format = RAW
 		log_group = root
@@ -75,8 +120,120 @@ sudo echo "log_file = /var/log/audit/audit.log
 		tcp_client_max_idle = 0
 		enable_krb5 = no
 		krb5_principal = auditd
-		##krb5_key_file = /etc/audit/audit.key" > /etc/audit/auditd.conf
-echo "Audit - "
+		##krb5_key_file = /etc/audit/audit.key" > tmp
+echo -n "."
+sudo cp tmp /etc/audit/auditd.conf
+echo "."
+sudo rm tmp
+echo "Done."
 
+echo -n "\nConfiguring sshd."
+sudo echo "# Package generated configuration file
+	# See the sshd_config(5) manpage for details
+
+	# What ports, IPs and protocols we listen for
+	Port 22
+	# Use these options to restrict which interfaces/protocols sshd will bind to
+	#ListenAddress ::
+	#ListenAddress 0.0.0.0
+	Protocol 2
+	# HostKeys for protocol version 2
+	HostKey /etc/ssh/ssh_host_rsa_key
+	HostKey /etc/ssh/ssh_host_dsa_key
+	HostKey /etc/ssh/ssh_host_ecdsa_key
+	HostKey /etc/ssh/ssh_host_ed25519_key
+	#Privilege Separation is turned on for security
+	UsePrivilegeSeparation yes
+
+	# Lifetime and size of ephemeral version 1 server key
+	KeyRegenerationInterval 3600
+	ServerKeyBits 1024
+
+	# Logging
+	SyslogFacility AUTH
+	LogLevel INFO
+
+	# Authentication:
+	LoginGraceTime 120
+	PermitRootLogin no
+	StrictModes yes
+
+	RSAAuthentication yes
+	PubkeyAuthentication yes
+	#AuthorizedKeysFile	%h/.ssh/authorized_keys
+
+	# Don't read the user's ~/.rhosts and ~/.shosts files
+	IgnoreRhosts yes
+	# For this to work you will also need host keys in /etc/ssh_known_hosts
+	RhostsRSAAuthentication no
+	# similar for protocol version 2
+	HostbasedAuthentication no
+	# Uncomment if you don't trust ~/.ssh/known_hosts for RhostsRSAAuthentication
+	#IgnoreUserKnownHosts yes
+
+	# To enable empty passwords, change to yes (NOT RECOMMENDED)
+	PermitEmptyPasswords no
+
+	# Change to yes to enable challenge-response passwords (beware issues with
+	# some PAM modules and threads)
+	ChallengeResponseAuthentication no
+
+	# Change to no to disable tunnelled clear text passwords
+	#PasswordAuthentication yes
+
+	# Kerberos options
+	#KerberosAuthentication no
+	#KerberosGetAFSToken no
+	#KerberosOrLocalPasswd yes
+	#KerberosTicketCleanup yes
+
+	# GSSAPI options
+	#GSSAPIAuthentication no
+	#GSSAPICleanupCredentials yes
+
+	X11Forwarding yes
+	X11DisplayOffset 10
+	PrintMotd no
+	PrintLastLog yes
+	TCPKeepAlive yes
+	#UseLogin no
+
+	#MaxStartups 10:30:60
+	#Banner /etc/issue.net
+
+	# Allow client to pass locale environment variables
+	AcceptEnv LANG LC_*
+
+	Subsystem sftp /usr/lib/openssh/sftp-server
+
+	# Set this to 'yes' to enable PAM authentication, account processing,
+	# and session processing. If this is enabled, PAM authentication will
+	# be allowed through the ChallengeResponseAuthentication and
+	# PasswordAuthentication.  Depending on your PAM configuration,
+	# PAM authentication via ChallengeResponseAuthentication may bypass
+	# the setting of "PermitRootLogin without-password".
+	# If you just want the PAM account and session checks to run without
+	# PAM authentication, then enable this but set PasswordAuthentication
+	# and ChallengeResponseAuthentication to 'no'.
+	UsePAM yes" > tmp
+echo -n "."
+sudo cp tmp /etc/ssh/sshd_config
+echo "."
+sudo rm tmp
+echo "Done."
+
+
+echo "\n\nSetting audit policies"
 sudo auditctl -e 1
-sudo apt-get --purge remove john
+echo "Done."
+echo "\nRemoving johntheripper"
+sudo apt --purge remove john
+echo "Done."
+echo "\nRemoving vsftpd"
+sudo apt --purge remove vsftpd
+echo "Done."
+
+echo "\n\nPossible backdoors: "
+sudo which nc
+sudo which netcat
+
